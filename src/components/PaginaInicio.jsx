d5ad4e6ae700cf35128b1d2c65f50fd86d589be4
Hugo Pacini <hugodpn@gmail.com>
2021-09-16 16:41:47 -0300

    Agregando useEffect

--- src/components/PaginaInicio.jsx
+++ src/components/PaginaInicio.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+const Materia = () => {
+
+    useEffect(()=> {
+        console.info("MOUNT: Mostrando materia...");
+
+        return () => {
+            console.info("UNMOUNT: Ocultando materia...");
+        }
+    }, []);
+
+    return <h2>Programacion 2</h2>;
+}
 
 const PaginaInicio = () => {
+
+    const [visible, setVisible] = useState(false);
+
+    useEffect(()=> {
+        console.info("MOUNT: Iniciando pagina de inicio...");
+
+        return () => {
+            console.info("UNMOUNT: Abandonando inicio...");
+        }
+    }, []);
+
     return (
         <div>
             <h1>Bienvenidos a CESIT</h1>
-            <h2>Programacion 2</h2>
+            <button onClick={()=> setVisible(!visible)}>Ver / Ocultar</button>
+            {visible && <Materia />}
         </div>
     )
 }
--- src/components/PaginaProductos.jsx
+++ src/components/PaginaProductos.jsx
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 const PaginaProductos = () => {
+
+  const [cargando, setCargando] = useState(false);
+  const [productos, setProductos] = useState([]);
+
+  const obtenerProductos = async () => {
+    setCargando(true);
+    const res = await axios.get('http://localhost:4000/productos');
+    setProductos(res.data);
+    setCargando(false);
+  }
+
+  useEffect(()=> {
+    console.info("MOUNT: Iniciando pagina de productos...");
+
+    obtenerProductos();
+    
+}, []);
+
+const renderProductos = () => {
+  return productos.map(prod => {
+    return (
+      <tr key={prod.id}>
+          <td>{prod.id}</td>
+          <td>{prod.nombre}</td>
+          <td>{prod.talle}</td>
+          <td>$ {prod.precio}</td>
+          <td><Link to={`/productos/${prod.id}`}>Editar</Link></td>
+      </tr>
+    )
+  })
+}
+
   return (
     <>
       <h2>Listando productos</h2>
-      <table>
+      {cargando && <b>Cargando...</b>}
+      {!cargando && <table>
           <thead>
               <tr>
                   <th>Id</th>
                   <th>Nombre</th>
+                  <th>Talle</th>
                   <th>Precio</th>
+                  <th>Acciones</th>
               </tr>
           </thead>
           <tbody>
-              <tr>
-                  <td>1</td>
-                  <td>Zapatillas</td>
-                  <td>$ 9420</td>
-              </tr>
-              <tr>
-                  <td>2</td>
-                  <td>Remera</td>
-                  <td>$ 2420</td>
-              </tr>
+              {renderProductos()}
           </tbody>
-      </table>
+      </table>}
     </>
   );
 };
