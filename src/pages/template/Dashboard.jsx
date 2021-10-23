1d49f6f33758eedfb6f6c61beeabd8884d813221
Hugo Pacini <hugodpn@gmail.com>
2021-10-22 21:31:04 -0300

    Actualiza template

--- src/pages/template/Dashboard.jsx
+++ src/pages/template/Dashboard.jsx
@@ -25,7 +25,7 @@ function Copyright(props) {
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
         Programación 3
-      </Link>{' '}
+      </Link>{' - '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
@@ -174,72 +174,3 @@ function DashboardContent() {
 export default function Dashboard() {
   return <DashboardContent />;
 }
-
-// import React from 'react';
-// import { createTheme, ThemeProvider } from '@mui/material/styles';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-// import Container from '@mui/material/Container';
-// import Grid from '@mui/material/Grid';
-// import Paper from '@mui/material/Paper';
-// import MyRouter from '../../MyRouter';
-// import TopMenu from './TopMenu';
-// import LeftMenue from './LeftMenue';
-
-// const drawerWidth = 240;
-// const mdTheme = createTheme();
-
-// const Dashboard = () => {
-//   const [open, setOpen] = React.useState(true);
-//   const toggleDrawer = () => {
-//     setOpen(!open);
-//   };
-
-//   return (
-//     <ThemeProvider theme={mdTheme}>
-//       <Box sx={{ display: 'flex' }}>
-//         <CssBaseline />
-//         <TopMenu
-//           open={open}
-//           drawerWidth={drawerWidth}
-//           mdTheme={mdTheme}
-//           toggleDrawer={toggleDrawer}
-//         />
-
-//         <LeftMenue 
-//           open={open}
-//           drawerWidth={drawerWidth}
-//           mdTheme={mdTheme}
-//           toggleDrawer={toggleDrawer}
-//         />
-
-//         <Box
-//           component='main'
-//           sx={{
-//             backgroundColor: (theme) =>
-//               theme.palette.mode === 'light'
-//                 ? theme.palette.grey[100]
-//                 : theme.palette.grey[900],
-//             flexGrow: 1,
-//             height: '100vh',
-//             overflow: 'auto',
-//           }}
-//         >
-//           <Toolbar />
-//           <Container maxWidth='lg' sx={{ mt: 4, mb: 4 }}>
-//             <Grid container spacing={3}>
-//               <Grid item xs={12}>
-//                 <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-//                   <MyRouter />
-//                 </Paper>
-//               </Grid>
-//             </Grid>
-//           </Container>
-//         </Box>
-//       </Box>
-//     </ThemeProvider>
-//   );
-// };
-
-// export default Dashboard;
