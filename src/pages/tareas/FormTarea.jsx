12fafef15df0fdd4e21ab5472955a6ba540071d0
Hugo Pacini <hugodpn@gmail.com>
2021-11-04 21:57:09 -0300

    Add styles to form

--- src/pages/tareas/FormTarea.jsx
+++ src/pages/tareas/FormTarea.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useForm } from 'react-hook-form';
+import { Link as RouterLink } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
 import {
   CrearTarea,
   GetTareaById,
   ActualizarTarea,
 } from '../../actions/tareasActions';
+import Title from '../../components/common/Title';
+import { TextField, Switch, FormControlLabel, Button, Grid, Alert } from '@mui/material';
 import { useHistory, useParams } from 'react-router';
 
 const FormTarea = () => {
   const {
-    register,
     handleSubmit,
     reset,
+    control,
+    register,
     formState: { errors },
   } = useForm();
   const dispatch = useDispatch();
@@ -52,30 +56,61 @@ const FormTarea = () => {
   };
 
   return (
-    <div>
-      <h2>{id ? 'Editando' : 'Nueva'} Tareas</h2>
+    <>
+      
+      <Title>{id ? 'Editando' : 'Nueva'} Tareas</Title>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor='nombre'>Nombre: </label>
-        <br />
-        <input type='text' {...register('nombre', { required: true })} />
-        {errors.nombre && <span>Este campo es requerido</span>}
-        <br />
 
-        <label htmlFor='descripcion'>Descripcion: </label>
-        <br />
-        <input type='text' {...register('descripcion')} />
-        <br />
+      <Grid container spacing={2}>
 
-        <label htmlFor='estaFinalizada'>Finalizada?: </label>
-        <br />
-        <input type='checkbox' {...register('estaFinalizada')} />
-        <br />
+        <Grid item xs={12}>
+          <Controller
+            render={({ field }) => <TextField label="Nombre" {...field} />}
+            name="nombre"
+            control={control}
+            defaultValue=""
+            className="materialUIInput"
+            rules={{ required: true }}
+          />
+          {errors.nombre && <Alert severity="error">Este campo es requerido!</Alert>}
+        </Grid>
 
-        <button type='submit'>Guardar</button>
-        <a href='/tareas'>Cancelar</a>
+        <Grid item xs={12}>
+
+          <Controller
+            render={({ field }) => <TextField multiline rows={4} label="Descripcion" {...field} />}
+            name="descripcion"
+            control={control}
+            defaultValue=""
+            className="materialUIInput"
+          />
+          </Grid>
+
+          <Grid item xs={12}>
+
+          <Controller
+            name='estaFinalizada'
+            defaultValue={false}
+            render={({ field: {onChange, value} }) => (
+              <FormControlLabel
+                control={<Switch checked={value} onChange={onChange} />}
+                label="Está finalizada?"
+              />
+            )}
+            control={control}
+          /> 
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button variant='contained' type='submit'>Guardar</Button>
+            &nbsp;
+            <Button variant='outlined' component={RouterLink} to='/tareas'>Cancelar</Button>
+          </Grid>
+
+        </Grid>  
       </form>
-    </div>
+    </>
   );
 };
 
